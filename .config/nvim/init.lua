e817357c1ca75d52f7a5d5820da1950dd9b300c4
Nicolas <89911360+d-kja@users.noreply.github.com>
2025-02-24 09:10:06 -0800

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,6 +9,12 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = true
 
+vim.g.netrw_banner = 0 -- Hide banner
+-- vim.g.netrw_browse_split = 4 -- Open in previous window
+-- vim.g.netrw_altv = 1 -- Open with right splitting
+vim.g.netrw_liststyle = 3 -- Tree-style view
+-- vim.g.netrw_list_hide = (vim.fn["netrw_gitignore#Hide"]()) .. [[,\(^\|\s\s\)\zs\.\S\+]] -- use .gitignore
+
 -- WARN: [[ Setting options ]]
 -- See `:help vim.opt`
 
